201040cac4da462a44194f2956f2dbc3605428cf
KYEONGMIN CHO <ggingmin@gmail.com>
2023-12-21 17:06:43 +0900

    commit

--- src/layouts/partials/Header.tsx
+++ src/layouts/partials/Header.tsx
@@ -78,43 +78,8 @@ const Header = () => {
         >
           {main.map((menu, i) => (
             <React.Fragment key={`menu-${i}`}>
-              {menu.hasChildren ? (
-                <li className="nav-item nav-dropdown group relative">
-                  <span
-                    className={`nav-link inline-flex items-center ${
-                      menu.children?.map(({ url }) => url).includes(pathname) ||
-                      menu.children
-                        ?.map(({ url }) => `${url}/`)
-                        .includes(pathname)
-                        ? "active"
-                        : ""
-                    }`}
-                  >
-                    {menu.name}
-                    <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </span>
-                  <ul className="nav-dropdown-list hidden group-hover:block lg:invisible lg:absolute lg:block lg:opacity-0 lg:group-hover:visible lg:group-hover:opacity-100">
-                    {menu.children?.map((child, i) => (
-                      <li className="nav-dropdown-item" key={`children-${i}`}>
-                        <Link
-                          href={child.url}
-                          className={`nav-dropdown-link block ${
-                            (pathname === `${child.url}/` ||
-                              pathname === child.url) &&
-                            "active"
-                          }`}
-                        >
-                          {child.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ) : (
                 <li className="nav-item">
-                  <Link
+                  <a
                     href={menu.url}
                     className={`nav-link block ${
                       (pathname === `${menu.url}/` || pathname === menu.url) &&
@@ -122,9 +87,8 @@ const Header = () => {
                     }`}
                   >
                     {menu.name}
-                  </Link>
+                  </a>
                 </li>
-              )}
             </React.Fragment>
           ))}
           {navigation_button.enable && (
